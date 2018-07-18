b8a1d15358809d858e17fcf3d12802c3bf4261a9
Brittney <brittfort34@gmail.com>
2018-07-18 04:32:09 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
 # Default is "World"
 # Author: Brittney Fortier (britt@somewhere.com)
-name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
