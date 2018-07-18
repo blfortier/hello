e53ca393c1c267328531a387508907e27866f01a
Brittney <brittfort34@gmail.com>
2018-07-18 03:20:26 +0000

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,6 +1,5 @@
 # Default is "World"
-
-"Hello, #{ARGV.first}!"
-# This is an unwanted but staged file
+# Author: Brittney Fortier (britt@somewhere.com)
 name = ARGV.first || "World"
+
 puts "Hello, #{name}!"
